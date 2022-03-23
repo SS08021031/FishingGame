22b36172555d089b0ee985167491366d2a6e1bbf
21cr0114 <21cr0114@jec.ac.jp>
2022-03-23 22:07:10 +0900

    PlayerControlの更新です。 スペースキーで回転します。

--- 開発/Assets/Scripts/PlayerControl.cs
+++ 開発/Assets/Scripts/PlayerControl.cs
@@ -13,6 +13,10 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0, 0, 3));
+        }
         
     }
 }
