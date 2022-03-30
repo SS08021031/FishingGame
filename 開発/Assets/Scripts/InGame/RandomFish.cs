ce074afe313be5587bc2ba8fbe03bf9af5d17e27
すぎしゅう <97151952+SS08021031@users.noreply.github.com>
2022-03-30 22:55:50 +0900

    魚が一定間隔で生成される

--- 開発/Assets/Scripts/InGame/RandomFish.cs
+++ 開発/Assets/Scripts/InGame/RandomFish.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
+
 public class RandomFish : MonoBehaviour
 {
     
-    public GameObject Fish;
+    public GameObject Fish; //Fishを格納
 
     public float X_min;　//Ｘ軸の最小値
     public float X_max;　//Ｘ軸の最大値
@@ -11,21 +11,23 @@ public class RandomFish : MonoBehaviour
     public float Y_min;　//Ｙ軸の最小値
     public float Y_max;　//Ｙ軸の最大値
 
+    public float instantiate;　//魚が生まれる時間
+   
+
     void Start()
     {
-        
+        InvokeRepeating("Instantiate", instantiate, instantiate);
+        //Instantiateメソッドを最初はinstantiate秒後、それ以降はinstantiate秒後に繰り返す
     }
 
-    void Update()
+    void Instantiate()
     {
-       
-       float x = Random.Range(X_min, X_max);　//Ｘ軸の最小値～最大値までの数値がランダムに出る
-       float y = Random.Range(Y_min, Y_max);　//Ｙ軸の最小値～最大値までの数値がランダムに出る
-            
-       Vector2 pos = new Vector2(x, y);　//出たＸ軸とＹ軸の数値を新しいVector2に代入
+        float x = Random.Range(X_min, X_max); //Ｘ軸の最小値～最大値までの数値がランダムに出る
+        float y = Random.Range(Y_min, Y_max); //Ｙ軸の最小値～最大値までの数値がランダムに出る
 
-       //新しいVector2の場所に生成する
-       Instantiate(Fish, pos, transform.rotation);
-              
+        Vector2 pos = new Vector2(x, y); //出たＸ軸とＹ軸の数値を新しいVector2に代入
+
+        //新しいVector2の場所に生成する
+        Instantiate(Fish, pos, transform.rotation);
     }
 }
